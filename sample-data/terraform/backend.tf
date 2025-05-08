34de0cf74e0d186392cb56c8f87cdb99ef4ac113
oscarpulido@google.com <oscarpulido@google.com>
2025-05-08 03:25:28 +0000

    team variables setup

--- sample-data/terraform/backend.tf
+++ sample-data/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "<TERRAFORM_BUCKET>"
-    prefix = "<TERRAFORM_ENV>"
+    bucket = "aef-aef-test-may-8-1-tfe"
+    prefix = "sample-data/environments/dev"
   }
 }
